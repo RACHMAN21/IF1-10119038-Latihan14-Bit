835651a49d7fa43f3f385c9d7d5e811d7be7c742
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 16:25:07 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan14/bit/IF110119038Latihan14Bit.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan14.bit;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF1
+ * NIM   : 10119038
+ * Deskripsi Program : Bit
+ * 
+ */
+
+public class IF110119038Latihan14Bit {
+    
+    public static void main(String[] args) {
+        int a = 60; /* 60 = 0011 1100 */
+        int b = 14; /* 13 = 0000 1101 */
+        int c = 0;
+        
+        c = a & b;
+        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c);
+        
+        c = a | b;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        
+        c = a ^ b;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+        
+        c = ~a;
+        /* -61 = 1100 0011 */
+        System.out.println("~a = " + c);
+        
+        c = a << 2;
+        /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c);
+        
+        c = a >> 2;
+        /* 15 = 0000 1111 */
+        System.out.println("a >> 2 = " + c);
+    }
+}
